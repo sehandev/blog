3d5467a280f67df1e907f62c179eb9c22bf69b0b
sehan <sehandev@gmail.com>
2020-07-26 23:56:29 +0900

    EDIT

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 # If a command fails then the deploy stops
 set -e
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo -t tranquilpeak-sehan
 
 # Go To Public folder
 cd sehandev.github.io
+
 # Add changes to git.
 git add .
 
 # Commit changes.
 msg="rebuilding site $(date)"
-if [ $# -eq 1 ]
-  then msg="$1"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 git commit -m "$msg"
 
